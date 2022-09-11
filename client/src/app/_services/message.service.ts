a6203dc53537d9ae7516ed18ebd0175ab8df79ad
chetanya gehlot <chetanya.gehlot@growexx.com>
2022-09-11 17:42:34 +0530

    Applied fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -8,6 +8,7 @@ import { User } from '../_models/user';
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { Group } from '../_models/group';
+import { BusyService } from './busy.service';
 
 @Injectable({
   providedIn: 'root'
@@ -19,9 +20,10 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private busyService:BusyService) { }
 
   createHubConnection(user: User,otherUsername: string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user='+ otherUsername, {
           accessTokenFactory: () => user.token
@@ -30,7 +32,8 @@ export class MessageService {
       .build();
 
     this.hubConnection.start()
-      .catch(error => console.log(error));
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread',messages => {
         this.messageThreadSource.next(messages);
@@ -58,6 +61,7 @@ export class MessageService {
 
   stopHubConnection(){
     if(this.hubConnection){
+      this.messageThreadSource.next([]);
       this.hubConnection.stop()
       .catch(error => console.log(error));
     }    
